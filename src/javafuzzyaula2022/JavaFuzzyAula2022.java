e20de194cac6dc260ee1dbb46759dddf48da7a08
Matheus Franco <matheusefranco@gmail.com>
2022-12-05 07:19:00 -0300

    fuzzy-aula

---
+++ src/javafuzzyaula2022/JavaFuzzyAula2022.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javafuzzyaula2022;
+
+/**
+ *
+ * @author Matheus
+ */
+public class JavaFuzzyAula2022 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmJFuzzyLogic form = new FrmJFuzzyLogic();
+        form.setVisible(true);
+    }
+    
+}
